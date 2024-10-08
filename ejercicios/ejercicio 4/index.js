ea1503c833dae98555f637cb244ffb74e1397b98
NouriHicham <156692558+NouriHicham@users.noreply.github.com>
2024-10-08 15:54:33 +0000

    pr4terminado

--- ejercicios/ejercicio 4/index.js
+++ ejercicios/ejercicio 4/index.js
@@ -1,14 +1,9 @@
-// document.getElementById("boton").addEventListener("click", function (event) {
-//     event.preventDefault();
-// });
-
-// document.querySelector(".btn").addEventListener("click", function(event){
-//     document.querySelector("#nombreNuevo").innerHTML = document.querySelector("#nombre").innerHTML
-//     document.querySelector("#apellidosNuevo").innerHTML = `${document.querySelector("#apellido1").innerHTML} ${document.querySelector("#apellido2").innerHTML}`
-// })
 
 document.getElementById("boton").addEventListener("click", function (event) {
-    document.querySelector("#nombreNuevo").innerHTML = document.querySelector("#nombre").innerHTML
-    document.querySelector("#apellidosNuevo").innerHTML = `${document.querySelector("#apellido1").innerHTML} ${document.querySelector("#apellido2").innerHTML}`
     event.preventDefault();
+    document.querySelector("#nombreNuevo").innerHTML = document.querySelector("#nombre").value
+    document.querySelector("#apellidosNuevo").innerHTML = ` ${document.querySelector("#apellido1").value} ${document.querySelector("#apellido2").value}`
+    document.querySelector("#dniNuevo").innerHTML = document.querySelector("#dni").value
+    let urlimg = document.querySelector("#url_img").value
+    document.querySelector(".card-img-top").setAttribute('src', urlimg);
 });
